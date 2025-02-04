7c564460f4fcb1bda31268ff57e46db589645469
Tran Quang Vinh <vinhtran@XPCQ4FWXVR.tld>
2025-02-04 11:42:19 -0500

    init_commit

---
+++ utils/configs.py
@@ -0,0 +1,18 @@
+def set_plot_configs():
+    from matplotlib import pyplot as plt
+    from matplotlib.ticker import MultipleLocator, MaxNLocator
+    import matplotlib as mpl
+
+    # define matplotlib style
+    mpl.style.use('classic')
+    mpl.rc('xtick', labelsize=23); mpl.rc('ytick', labelsize=23)
+    mpl.rc('xtick.major', size=15 , width=2)
+    mpl.rc('xtick.minor', size=8, width=2, visible=True)
+    mpl.rc('ytick.major', size=15 , width=2)
+    mpl.rc('ytick.minor', size=8, width=2, visible=True)
+    mpl.rc('lines',linewidth=3, markersize=20)
+    mpl.rc('axes', linewidth=2, labelsize=30, labelpad=2.5)
+    mpl.rc('legend', fontsize=25, loc='best', frameon=False, numpoints=1)
+
+    mpl.rc('font', family='STIXGeneral')
+    mpl.rc('mathtext', fontset='stix')
