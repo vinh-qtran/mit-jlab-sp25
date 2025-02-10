4bfa832e48574b119a48cc2d55566180989a9ddb
Tran Quang Vinh <vinhtran@XPCQ4FWXVR.tld>
2025-02-09 23:59:32 -0500

    Add XS1 & Update modules.utils

--- modules/utils.py
+++ modules/utils.py
@@ -23,10 +23,10 @@ def check_array(array,log=False):
     if log:
         array = np.log10(array)
 
-    print("  Shape:",array.shape)
+    print(" Shape:",array.shape)
     if np.any(np.isnan(array)):
-        print("  NaNs: ",np.sum(np.isnan(array)))
-    print("  Min:  ",np.nanmin(array))
-    print("  Max:  ",np.nanmax(array))
-    print("  Mean: ",np.nanmean(array))
-    print("  Std:  ",np.nanstd(array))
+        print(" NaNs: ", f'{np.sum(np.isnan(array))}')
+    print(" Min:  ", f'{np.nanmin(array):.2e}')
+    print(" Max:  ", f'{np.nanmax(array):.2e}')
+    print(" Mean: ", f'{np.nanmean(array):.2e}')
+    print(" Std:  ", f'{np.nanstd(array):.2e}')
